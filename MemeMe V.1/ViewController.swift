974967c0645cbd732a108a3be2088b9c516a77f2
chezze911 <chinguyen@chis-mbp.attlocal.net>
2017-01-22 14:00:28 -0800

    Fix bug by adding connection to IBoutlet and Inaction for camera
    created camera button on toolbar to make proper connections

--- MemeMe V.1/ViewController.swift
+++ MemeMe V.1/ViewController.swift
@@ -11,54 +11,56 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
 UINavigationControllerDelegate {
     
-    let memeTextAttributes = [
-        NSStrokeColorAttributeName : UIColor.blackColor(),
-        NSForegroundColorAttributeName: UIColor.whiteColor(),
-        NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-        NSStrokeWidthAttributeName : -4.0
-    ]
 
-    @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var imagePickerView: UIImageView!
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)
-    }
-    
+//    let memeDelegate = MemeTextFieldDelegate()
+//    
+//    let memeTextAttributes = [
+//        NSStrokeColorAttributeName : UIColor.blackColor(),
+//        NSForegroundColorAttributeName: UIColor.whiteColor(),
+//        NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+//        NSStrokeWidthAttributeName : -4.0
+//    ]
+
+    // Do any additional setup after loading the view, typically from a nib.
     override func viewDidLoad() {
         super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-        
     }
-
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+         //enables camera button only if camera is available
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)
+    }
+    //opens camera roll to allow photo selection
     @IBAction func pickAnImage(_ sender:Any) {
-        
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
         pickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
-        self.present(pickerController, animated: true, completion: nil)
+        present(pickerController, animated: true, completion: nil)
     }
-    func imagePickerController(_: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            self.imagePickerView.image = image
-            self.dismiss(animated: true, completion: nil)
-            //Tells the delegate that the user picked a still image
-        }
-    }
-    func imagePickerControllerDidCancel(_: UIImagePickerController) {
-        self.dismiss(animated: true, completion: nil)
-        //Tells the delegate that the user cancelled the pick operation.
-    }
+    //enables user to take a photo
+    
     @IBAction func pickAnImageFromCamera(_ sender: Any) {
-        
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
         pickerController.sourceType = .camera
         present(pickerController, animated: true, completion: nil)
     }
+    //Tells the delegate that the user picked a still image
+    //and sends photo user selected to image viewer and formats to aspect fit
+    func imagePickerController(_: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.image = image
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    //Tells the delegate that the user cancelled the pick operation.
+    func imagePickerControllerDidCancel(_: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 
 }
 
