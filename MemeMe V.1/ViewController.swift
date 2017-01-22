204e0c6157de5b1264822d2c1237dbe27a8a5deb
chezze911 <chinguyen@chis-mbp.attlocal.net>
2017-01-21 22:15:19 -0800

    Added UI Image Picker Controller Delegate

--- MemeMe V.1/ViewController.swift
+++ MemeMe V.1/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
 
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func pickAnImage(_ sender:Any) {
+        
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        self.present(pickerController, animated: true, completion: nil)
     }
-
-
+    func imagePickerController(_: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.imagePickerView.image = image
+            self.dismiss(animated: true, completion: nil)
+        }
+    
+    //Tells the delegate that the user picked a still image 
+    }
+    func imagePickerControllerDidCancel(_: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    //Tells the delegate that the user cancelled the pick operation.
 }
 
